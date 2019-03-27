2f6bddd7bc06aa1825d1f7a52267d6ddae54298b
topix <atrline1@gmail.com>
2019-03-27 23:51:37 +0700

    topix

---
+++ app/src/main/java/com/example/wakhid/pendaftaran/Value.java
@@ -0,0 +1,14 @@
+package com.example.wakhid.pendaftaran;
+
+public class Value {
+    String value;
+    String message;
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
